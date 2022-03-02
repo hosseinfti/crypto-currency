2fc570e404f4945383346e71bd2ba594bdea014d
Hossein <hosseinfti@gmail.com>
2022-03-02 23:25:36 +0330

    Updates

---
+++ precache-manifest.4f6f5ca245dd3c5c62a7ba3815c57c70.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a94422dd44af52438da8cd1b7c01fe9",
+    "url": "/crypto-currency/index.html"
+  },
+  {
+    "revision": "3f0747f29efd9d3459fd",
+    "url": "/crypto-currency/static/css/main.88ff24b6.chunk.css"
+  },
+  {
+    "revision": "4374c70050e843321e05",
+    "url": "/crypto-currency/static/js/2.b87ffe85.chunk.js"
+  },
+  {
+    "revision": "a90bb751b102ed9d8f9443a23ededd32",
+    "url": "/crypto-currency/static/js/2.b87ffe85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f0747f29efd9d3459fd",
+    "url": "/crypto-currency/static/js/main.d14a079f.chunk.js"
+  },
+  {
+    "revision": "764bcb8fc9ba7951c13b",
+    "url": "/crypto-currency/static/js/runtime-main.7e075acc.js"
+  },
+  {
+    "revision": "16bd1ca84f73ca4dfbb637199cbcf40d",
+    "url": "/crypto-currency/static/media/ADA.16bd1ca8.svg"
+  },
+  {
+    "revision": "e397ec04827e0f204bb750116fccd3a0",
+    "url": "/crypto-currency/static/media/ALICE.e397ec04.svg"
+  },
+  {
+    "revision": "fa6fbde2a8c029cd826b9613f519825d",
+    "url": "/crypto-currency/static/media/ATOM.fa6fbde2.svg"
+  },
+  {
+    "revision": "d2409998d06e347a214456d262685c05",
+    "url": "/crypto-currency/static/media/AXS.d2409998.svg"
+  },
+  {
+    "revision": "8653be47a0c99a5e17f7af7b10abdede",
+    "url": "/crypto-currency/static/media/BCH.8653be47.svg"
+  },
+  {
+    "revision": "40d26c1f1e86944acd18243413464da2",
+    "url": "/crypto-currency/static/media/BNB.40d26c1f.svg"
+  },
+  {
+    "revision": "45174c5ccb56d426cc4b4ad3a5f03fff",
+    "url": "/crypto-currency/static/media/BTC.45174c5c.svg"
+  },
+  {
+    "revision": "1a8ed37257271ce699c7a91b78b693e6",
+    "url": "/crypto-currency/static/media/BTT.1a8ed372.svg"
+  },
+  {
+    "revision": "083df9273a6e3ca3eff768fe64960b3e",
+    "url": "/crypto-currency/static/media/CAKE.083df927.svg"
+  },
+  {
+    "revision": "c0ce76a7a03b5817da1fcf5bbe38953a",
+    "url": "/crypto-currency/static/media/CHZ.c0ce76a7.svg"
+  },
+  {
+    "revision": "ee6d808db4e77aa1f12b0923dc5817cb",
+    "url": "/crypto-currency/static/media/DASH.ee6d808d.svg"
+  },
+  {
+    "revision": "91137c49d6635f1cad52a499e98e34fb",
+    "url": "/crypto-currency/static/media/DOGE.91137c49.svg"
+  },
+  {
+    "revision": "bc8421634ae9fabad753aace954f1f64",
+    "url": "/crypto-currency/static/media/DOT.bc842163.svg"
+  },
+  {
+    "revision": "9deab7469ad47fc6cf713311ed9294eb",
+    "url": "/crypto-currency/static/media/ENJ.9deab746.svg"
+  },
+  {
+    "revision": "45b46329581015dc0c697a76362e838b",
+    "url": "/crypto-currency/static/media/EOS.45b46329.svg"
+  },
+  {
+    "revision": "58d1c3a7c7339067f44ea71981d95b99",
+    "url": "/crypto-currency/static/media/ETH.58d1c3a7.svg"
+  },
+  {
+    "revision": "65475ae4ef29769e0b699fffe210c497",
+    "url": "/crypto-currency/static/media/Estedad-Bold.65475ae4.ttf"
+  },
+  {
+    "revision": "8dcf404349d3f5dd9c309aa21b712f71",
+    "url": "/crypto-currency/static/media/Estedad-Regular.8dcf4043.ttf"
+  },
+  {
+    "revision": "0240620953703c74166e466f0a1e420c",
+    "url": "/crypto-currency/static/media/Estedad-Thin.02406209.ttf"
+  },
+  {
+    "revision": "84facaf9777c18cd727357749c25399f",
+    "url": "/crypto-currency/static/media/FIL.84facaf9.svg"
+  },
+  {
+    "revision": "39c688df120e9a7b5d95175a1c6ae82e",
+    "url": "/crypto-currency/static/media/FTM.39c688df.svg"
+  },
+  {
+    "revision": "6080a28217dda7d56cfba98f35331908",
+    "url": "/crypto-currency/static/media/LINK.6080a282.svg"
+  },
+  {
+    "revision": "ad57e1aa362e9e74894dbc9cd9c28fa0",
+    "url": "/crypto-currency/static/media/LTC.ad57e1aa.svg"
+  },
+  {
+    "revision": "418ae32308cc1f1744dfb8e9bdbb5132",
+    "url": "/crypto-currency/static/media/MANA.418ae323.svg"
+  },
+  {
+    "revision": "96fee1c645dc3f31e95930494c2f19f1",
+    "url": "/crypto-currency/static/media/MATIC.96fee1c6.svg"
+  },
+  {
+    "revision": "2b7d11595295fc9e4d730460c33edcf9",
+    "url": "/crypto-currency/static/media/RUNE.2b7d1159.svg"
+  },
+  {
+    "revision": "a6699807f0498eb9f53c95144d5b35da",
+    "url": "/crypto-currency/static/media/SAND.a6699807.svg"
+  },
+  {
+    "revision": "dac7ae04e8a8de9b06764050e70f4b49",
+    "url": "/crypto-currency/static/media/SHIB.dac7ae04.svg"
+  },
+  {
+    "revision": "c1d72708b08548d0312631aff57c46f6",
+    "url": "/crypto-currency/static/media/TMN.c1d72708.svg"
+  },
+  {
+    "revision": "e80f346a2830df7e6293e9b0238bc35f",
+    "url": "/crypto-currency/static/media/TRX.e80f346a.svg"
+  },
+  {
+    "revision": "006061313d4c6f30a66b49e9377df0fc",
+    "url": "/crypto-currency/static/media/UNI.00606131.svg"
+  },
+  {
+    "revision": "9000544e71872f3a9f1470e38f9a7394",
+    "url": "/crypto-currency/static/media/USDT.9000544e.svg"
+  },
+  {
+    "revision": "65dd1a8cc7794e8cb9aa78a0b6a984f8",
+    "url": "/crypto-currency/static/media/XLM.65dd1a8c.svg"
+  },
+  {
+    "revision": "4c2171e859cf844d2eb3bda7dce9b73b",
+    "url": "/crypto-currency/static/media/XRP.4c2171e8.svg"
+  },
+  {
+    "revision": "2b0f93b21e1272c2bb2492753eb5d83b",
+    "url": "/crypto-currency/static/media/money-exchange.2b0f93b2.png"
+  },
+  {
+    "revision": "e7081ba49137ae8ee7214c3eac8b815a",
+    "url": "/crypto-currency/static/media/starOff.e7081ba4.png"
+  },
+  {
+    "revision": "f9fc7156de4815eef6d710b986b30bbf",
+    "url": "/crypto-currency/static/media/starOn.f9fc7156.png"
+  },
+  {
+    "revision": "137c3251bde1becbc075f20dce554004",
+    "url": "/crypto-currency/static/media/undraw_page_not_found_re_e9o6.137c3251.svg"
+  }
+]);

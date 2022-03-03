6ab70817ed1b0e277770dabe61c89ee454c0e3a6
Hossein <hosseinfti@gmail.com>
2022-03-03 18:23:33 +0330

    fix favIcon

--- src/pages/not-found/NotFound.js
+++ src/pages/not-found/NotFound.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./not-found.css"
+import "./not-found.css";
 import { Helmet } from "react-helmet";
 
 function NotFound() {
@@ -12,13 +12,14 @@ function NotFound() {
         alt="notFound"
       />
       <Link className="returnHomeLink" to="/">
-        <div className="returnHome">
-            بازگشت به صفحه‌ی اصلی
-        </div>
+        <div className="returnHome">بازگشت به صفحه‌ی اصلی</div>
       </Link>
       <div className="notFoundFA"> صفحه مورد نظر یافت نشد! </div>
       <div className="notFoundEN">Error 404 - Page Not Found</div>
-      <Helmet> <title> صفحه مورد نظر یافت نشد | BestCoino </title> </Helmet>
+      <Helmet>
+        {" "}
+        <title> صفحه مورد نظر یافت نشد | BestCoino </title>
+      </Helmet>
     </div>
   );
 }

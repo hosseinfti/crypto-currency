56a11508b6db89443f79949665cf0c35d5211fdc
Hossein <hosseinfti@gmail.com>
2022-03-03 13:16:18 +0330

    change router to hash

--- src/pages/router.js
+++ src/pages/router.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Finance from "../Finance";
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
